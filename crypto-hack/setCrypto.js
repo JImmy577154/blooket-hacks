5a169575571c1e8f052de428254387b2a6948f6b
JImmy577154 <robertcoopercallaway@icloud.com>
2025-02-11 13:21:44 -0600

    Update setCrypto.js

--- crypto-hack/setCrypto.js
+++ crypto-hack/setCrypto.js
@@ -15,7 +15,7 @@
                 let amount = Number(parseFloat(prompt('How much crypto do you want?')));
                 reactHandler().stateNode.setState({ crypto2: amount, crypto: amount });
         
-                alert('Crypto added!');
+                alert('12345678765432123456786543212345676543212345678976543234567!');
             };
 })();
 
